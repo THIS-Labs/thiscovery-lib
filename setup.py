342071676b18fecd48a7b1c2f8e28e7446c5d796
André Sartori <afs25@cam.ac.uk>
2021-01-13 16:14:43 +0000

    Fixed typo in appointments-by-type endpoint

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thiscovery-lib",  # Replace with your own username
-    version="2021.1.4",
+    version="2021.1.5",
     author="Thiscovery team",
     author_email="support@thiscovery.org",
     description="Thiscovery library",
