79983107966e7f25d37593a5bd89481c41e4aadd
André Sartori <afs25@cam.ac.uk>
2021-05-21 22:55:03 +0100

    Added ssm_utilities.py

---
+++ thiscovery_lib/ssm_utilities.py
@@ -0,0 +1,35 @@
+#
+#   Thiscovery API - THIS Institute’s citizen science platform
+#   Copyright (C) 2019 THIS Institute
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   A copy of the GNU Affero General Public License is available in the
+#   docs folder of this project.  It is also available www.gnu.org/licenses/
+#
+from __future__ import annotations
+import thiscovery_lib.utilities as utils
+
+
+class SsmClient(utils.BaseClient):
+    def __init__(self):
+        super().__init__("ssm")
+
+    def get_parameters(self, names: list) -> dict[str, list]:
+        """
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ssm.html#SSM.Client.get_parameters
+
+        Args:
+            names (list): Names of the parameters for which you want to query information
+
+        Returns:
+        """
+        return self.client.get_parameters(Names=names)
