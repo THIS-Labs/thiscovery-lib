0cc2496c11e6995a521d92be898d00da2b167992
Sophie Machin <sophie.machin@thislabs.org>
2023-06-23 08:55:28 +0100

    tidy up

--- tests/test_thiscovery_api_utils/test_core_api_utilities.py
+++ tests/test_thiscovery_api_utils/test_core_api_utilities.py
@@ -15,7 +15,7 @@
 #   A copy of the GNU Affero General Public License is available in the
 #   docs folder of this project.  It is also available www.gnu.org/licenses/
 #
-import local.dev_config  # sets env variables TEST_ON_AWS and AWS_TEST_API	try:
+import local.dev_config  # sets env variables TEST_ON_AWS and AWS_TEST_API
 import local.secrets  # sets env variables THISCOVERY_AFS25_PROFILE and THISCOVERY_AMP205_PROFILE
 
 import json
--- thiscovery_lib/utilities.py
+++ thiscovery_lib/utilities.py
@@ -425,6 +425,7 @@ class SsmClient(BaseClient):
 class SecretsManager(BaseClient):
     def __init__(self, profile_name=None):
         super().__init__("secretsmanager", profile_name=profile_name)
+
     def _prefix_name(self, name, prefix):
         if prefix is None:
             prefix = f"/{super().get_namespace()}/"
